07e2ac28457a4624c35bf84a8bffad538976c0a5
Alan Marchesan <justcavalli@gmail.com>
2016-07-20 06:58:16 +0200

    Add Clean thumbnails functionality - credits to Koying PR -> https://github.com/christiantroy/xbmc/pull/3

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -366,6 +366,7 @@ public:
   static const std::string SETTING_GENERAL_ADDONNOTIFICATIONS;
   static const std::string SETTING_GENERAL_ADDONFOREIGNFILTER;
   static const std::string SETTING_GENERAL_ADDONBROKENFILTER;
+  static const std::string SETTING_THUMBNAILS_CLEANUP;
 
   /*!
    \brief Creates a new settings wrapper around a new settings manager.
